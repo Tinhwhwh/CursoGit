0312c60132a92d182d9ed78546e6a1e94333d834
Valentin Carlesso <valentin.carlesso@unvierso.univates.br>
2024-09-08 22:58:22 -0300

    criando uma branch com os mesmos textos porem em javascript
    literalmente, so isso

---
+++ Complicacao.js
@@ -0,0 +1,5 @@
+console.log("Este e o commit numero 1 pra ver se eu realmente entendi as aulas que vi");
+
+console.log("esta linha rerpesenta meu segundo commit, pois nela foi realizado meu segundo commit");
+
+console.log("terceiro commit! olha so, nao e tao dificil assim!");
